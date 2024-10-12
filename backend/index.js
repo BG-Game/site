299ae1951774e8f61e05fd6fb477f376dc189063
BG_Game <you@example.com>
2024-10-12 17:32:51 +0300

    2

--- backend/index.js
+++ backend/index.js
@@ -2,23 +2,28 @@ const express = require('express')
 var app = express();
 const cors = require('cors')
 const PORT = 3001
-
-const corsOptions = {
-    origin:  ["http://localhost:3000"],
-};
-
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/TD_Users')
 
-const  usersSchema = new mongoose.Schema({
+app.use(cors());
+
+app.use(express.json()); // enable JSON parsing
+
+mongoose.connect('mongodb://127.0.0.1:27017/rasp_db')
+
+const  dbgroupsSchema = new mongoose.Schema({
     name: String,
-    age:  Number,
-    pas:  String
+    rasp: [{
+        one_week: Object,
+        two_weeks: Object,
+    }]
 })
 
-const UsersModel = mongoose.model('TD_Users', usersSchema)
+const dbweekSchema = new mongoose.Schema({
+    week: String
+})
 
-app.use(cors(corsOptions))
+const dbweekModel = mongoose.model('week', dbgroupsSchema)
+const dbGroupsModel = mongoose.model('rasp', dbgroupsSchema)
 
 app.get('/api', (req, res) => {
     res.json({
@@ -26,14 +31,57 @@ app.get('/api', (req, res) => {
     })
 })
 
-app.get('/getUsers', (req, res) => {
-    const data = UsersModel.find({}).then(function(users){
+app.get('/getWeek', (req, res) => {
+    console.log("getWeek");
+    const data = dbweekModel.find({}).then(function(users){
         res.json(users)
     }).catch(function(err) {
         console.log(err);
     })
 })
 
+app.get('/getData', (req, res) => {
+    console.log("getGroups");
+    const data = dbGroupsModel.find({}).then(function(users){
+        res.json(users)
+    }).catch(function(err) {
+        console.log(err);
+    })
+})
+
+app.get('/getData/:id', (req, res) => {
+    // res.send(`<h1>${req.params.id}</h1>`);
+    // res.json({
+    //     message: req.params.id
+    // })
+    console.log("getData/" + req.params.id);
+    const data = dbGroupsModel.find({name: req.params.id}).then(function(users){
+        res.json(users)
+    }).catch(function(err) {
+        console.log(err);
+    })
+});
+
+app.get('/getGroups', (req, res) => {
+    console.log("getGroups");
+    const data = dbGroupsModel.find({}).then(function(users){
+        res.json(users)
+    }).catch(function(err) {
+        console.log(err);
+    })
+})
+
+// app.post('/log', (req, res) => {
+//     const { login, pass } = req.body;
+//     console.log(`Received text: ${login.login} , ${pass.pass}`);
+//     res.send(true);
+// });
+
+// app.post('/test', (req, res) => {
+//     const asd = req.body.asd;
+//     console.log(`Received text: ${asd}`);
+//     res.send(true);
+// });
 
 app.listen(PORT, () => {
     console.log("Server start PORT: " + PORT);
